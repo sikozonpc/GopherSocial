349bfb52539376347961fa76613bd993e61328f3
GitHub Action <action@github.com>
2024-09-10 13:59:12 +0000

    Update version to 1.1.0

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,7 +16,7 @@ import (
 	"go.uber.org/zap"
 )
 
-const version = "0.0.1"
+const version = "1.1.0"
 
 //	@title			GopherSocial API
 //	@description	API for GopherSocial, a social network for gohpers
